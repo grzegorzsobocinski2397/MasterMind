d58412d56b7e241abc5a40d97d05273defbd61d8
Grzegorz Sobocinski <grzegorz.sobocinski2397@outlook.com>
2019-12-24 13:51:33 +0100

    Create User class

---
+++ MasterMind/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterMind.Models
+{
+    public class User
+    {
+        #region Public Propeties
+        /// <summary>
+        /// Information about how many games user has won.
+        /// </summary>
+        public int GamesWon { get; set; }
+        /// <summary>
+        /// Information about how many games user has lost.
+        /// </summary>
+        public int GamesLost { get; set; }
+        /// <summary>
+        /// Contains current's round answers.
+        /// </summary>
+        public List<char[]> CurrentAnswers { get; set; }
+        #endregion
+    }
+}
