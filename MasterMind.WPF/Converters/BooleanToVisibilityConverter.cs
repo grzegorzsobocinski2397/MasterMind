64ce75e472505a60202b5c1580bfcc9e56446f41
Grzegorz Sobocinski <grzegorz.sobocinski2397@gmail.com>
2019-12-25 14:56:16 +0100

    Add error check in WPF

---
+++ MasterMind.WPF/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace MasterMind.WPF
+{
+    /// <summary>
+    /// Value converter that converts bool into a Visibility value.
+    /// </summary>
+    public class BooleanToVisibilityConverter : BaseValueConverter<BooleanToVisibilityConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value == false)
+                return Visibility.Hidden;
+            else if ((bool)value == true)
+                return Visibility.Visible;
+            else
+                return null;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MasterMind.WPF/ViewModels/GameViewModel.cs
+++ MasterMind.WPF/ViewModels/GameViewModel.cs
@@ -1,22 +1,68 @@
 ﻿using MasterMind.WPF.ViewModels.Base;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows.Input;
 
 namespace MasterMind.WPF.ViewModels
 {
-    class GameViewModel : BaseViewModel
+    internal class GameViewModel : BaseViewModel
     {
+        #region Constant Fields
+
+        /// <summary>
+        /// Contains all the available colors in the game Mastermind..
+        /// </summary>
+        private const string AvailableColorsString = "rygbmc";
+
+        #endregion Constant Fields
+
+        #region Private Fields
+
+        /// <summary>
+        /// Instance of the Mastermind game.
+        /// </summary>
+        private Game game;
+
+        #endregion Private Fields
+
+        #region Public Properties
+
+        /// <summary>
+        /// Contains the information about current round.
+        /// </summary>
+        public string RoundInformation { get; set; }
+
+        /// <summary>
+        /// Contains the player's input.
+        /// </summary>
+        public string InputValue { get; set; }
+
+        /// <summary>
+        /// Contains information about what player did wrong.
+        /// </summary>
+        public string ErrorInformation { get; set; }
+
+        /// <summary>
+        /// User typed in incorrect input.
+        /// </summary>
+        public bool IsInputWrong { get; set; }
+
+        #endregion Public Properties
+
         #region Commands
 
         /// <summary>
         /// User clicked the 'x' icon.
         /// </summary>
         public ICommand CloseCommand { get; set; }
-        #endregion
+
+        /// <summary>
+        /// User clicked the 'go' icon. Send the answer to Game.
+        /// </summary>
+        public ICommand SendInputCommand { get; set; }
+
+        #endregion Commands
+
         #region Constructors
 
         /// <summary>
@@ -25,9 +71,14 @@ namespace MasterMind.WPF.ViewModels
         public GameViewModel()
         {
             CloseCommand = new RelayCommand(() => CloseApplication());
+            SendInputCommand = new RelayCommand(() => CheckInput());
+            game = new Game();
+            RoundInformation = GetRoundInformationText();
         }
-        #endregion
-        #region Public Methods
+
+        #endregion Constructors
+
+        #region Private Methods
 
         /// <summary>
         /// Close the application window.
@@ -36,6 +87,42 @@ namespace MasterMind.WPF.ViewModels
         {
             Environment.Exit(0);
         }
-        #endregion
+
+        private void SendInput()
+        {
+            game.CheckCode(InputValue);
+            RoundInformation = GetRoundInformationText();
+        }
+
+        private void CheckInput()
+        {
+            if (InputValue.Length != game.CodeLength)
+            { 
+                SetError("Length of the input was incorrect.");
+                return;
+            }
+
+            bool doCharactersExist = !Regex.IsMatch(InputValue, $"[^{AvailableColorsString}]");
+
+            if (doCharactersExist)
+            {
+                SetError("There were incorrect characters written.");
+                return;
+            }
+
+            SendInput();
+        }
+        private void SetError(string error)
+        {
+            IsInputWrong = true;
+            ErrorInformation = error;
+        }
+
+        private string GetRoundInformationText()
+        {
+            return $"Próba {game.Round}/{game.TimeLimit}. Ilość znaków w kodzie: {game.CodeLength}";
+        }
+
+        #endregion Private Methods
     }
 }
